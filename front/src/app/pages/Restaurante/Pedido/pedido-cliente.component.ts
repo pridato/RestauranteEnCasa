80f11c563779d7012bf853e23b9dfd646aa95fdc
pridato <davidarroyo25052004@gmail.com>
2024-05-09 18:08:59 +0200

    creado stepper de pagos y estado del pedido al usuario. Pendiente implementar lógica

---
+++ front/src/app/pages/Restaurante/Pedido/pedido-cliente.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+
+@Component({
+  selector: 'app-pedido-cliente',
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: {displayDefaultIndicatorType: false},
+    },
+  ],
+  standalone: true,
+  imports: [
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  templateUrl: './pedido-cliente.component.html',
+  styleUrl: './pedido-cliente.component.css'
+})
+export class PedidoClienteComponent {
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+
+  constructor(private _formBuilder: FormBuilder) {}
+}
--- front/src/app/pages/Restaurante/comida.route.ts
+++ front/src/app/pages/Restaurante/comida.route.ts
@@ -3,11 +3,15 @@ import { ComidasComponent } from "./Comidas/comidas.component";
 import { DetallecomidaComponent } from "./Comidas/DetalleComida/detallecomida.component";
 import { ReservasComponent } from "./Reservas/reservas.component";
 import { ComidascompradasComponent } from "./Comidas/ComidasCompradas/comidascompradas.component";
+import { PedidoClienteComponent } from "./Pedido/pedido-cliente.component";
 
 
 export const COMIDA_ROUTES: Routes = [
     { path: 'Comidas', component: ComidasComponent},
     {path: 'Reservas', component: ReservasComponent},
     {path: 'Detalle/:id', component: DetallecomidaComponent},
-    {path: 'Pedido', component: ComidascompradasComponent}
+    {path: 'Gustados', component: ComidascompradasComponent},
+    {path: 'Pedido', component: PedidoClienteComponent}
+    
 ]
+
