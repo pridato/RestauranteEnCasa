6f250d6509e55624f2a0e2f7e8bbc0d08cf4130a
pridato <davidarroyo25052004@gmail.com>
2024-05-11 17:20:32 +0200

    añadido reservar solo fechas actuales o posteriores

--- front/src/app/pages/Restaurante/Reservas/reservas.component.ts
+++ front/src/app/pages/Restaurante/Reservas/reservas.component.ts
@@ -3,6 +3,7 @@ import {MatDatepickerModule} from '@angular/material/datepicker'
 import {MatCardModule} from '@angular/material/card';
 import {provideNativeDateAdapter} from '@angular/material/core';
 import { GuardarFechaComponent } from './dialogGuardar/guardar-fecha.component';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-reservas',
@@ -18,8 +19,20 @@ export class ReservasComponent {
 
   mostrarDialog:boolean = false
 
+  constructor(private toastr:ToastrService) {}
+
+  handleDateChange(event: Date) {
+    // si la fecha no es anterior a la actual
+    if (event >= new Date()) {
+      this.selected = event
+      this.mostrarDialog = true
+    } else {
+      this.toastr.error('La fecha seleccionada no puede ser anterior a la actual', 'Error reservando')
+    }
+    
+  }
+
   recibirDialog(event:boolean) {
-    console.log(event)
     this.mostrarDialog = event
   }
 
