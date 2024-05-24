a99cd373f871593f26ad52421657e7614ec650e6
pridato <davidarroyo25052004@gmail.com>
2024-05-24 11:46:43 +0200

    eliminado spinner

--- front/src/app/pages/Dashboard/camarero/camarero-dashboard.component.ts
+++ front/src/app/pages/Dashboard/camarero/camarero-dashboard.component.ts
@@ -1,73 +1,83 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-import { ComidaPedido } from 'src/app/core/models/ComidaPedido';
-import { ICliente } from 'src/app/core/models/cliente';
-import { Pedido } from 'src/app/core/models/pedido';
-import { RestService } from 'src/app/core/servicios/RestService.service';
-import { CamareroDashboardService } from 'src/app/core/servicios/camarero-dashboard.service';
-import { SpinnerComponent } from 'src/app/shared/componentes/spinner/spinner.component';
-import { mesas } from 'src/app/shared/globales/globales';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
+import { ComidaPedido } from "src/app/core/models/ComidaPedido";
+import { ICliente } from "src/app/core/models/cliente";
+import { Pedido } from "src/app/core/models/pedido";
+import { RestService } from "src/app/core/servicios/RestService.service";
+import { CamareroDashboardService } from "src/app/core/servicios/camarero-dashboard.service";
+import { SpinnerComponent } from "src/app/shared/componentes/spinner/spinner.component";
+import { mesas } from "src/app/shared/globales/globales";
 
 @Component({
-  selector: 'app-camarero-dashboard',
+  selector: "app-camarero-dashboard",
   standalone: true,
   imports: [CommonModule, SpinnerComponent],
-  templateUrl: './camarero-dashboard.component.html',
-  styleUrl: './camarero-dashboard.component.css'
+  templateUrl: "./camarero-dashboard.component.html",
+  styleUrl: "./camarero-dashboard.component.css",
 })
 export class CamareroDashboardComponent {
+  mesas: number[] = mesas;
+  pedidos: Pedido[] = [];
 
-  mesas:number[] = mesas
-  pedidos:Pedido[] = []
+  mesasOcupadas: number[] = [];
 
-  mesasOcupadas:number[] = []
-
-  constructor(private camareroSvc: CamareroDashboardService, private rest:RestService, private toastr:ToastrService) {
+  constructor(
+    private camareroSvc: CamareroDashboardService,
+    private rest: RestService,
+    private toastr: ToastrService
+  ) {
     // every 20 seconds
-    this.getPedidos()
+   
+    this.getPedidos();
     setInterval(() => {
-      this.getPedidos()
-    }, 20000)
+      this.getPedidos();
+    }, 20000);
   }
 
   /**
    * metodo para obtener los pedidos de la base de datos
    */
   getPedidos() {
-    this.camareroSvc.cargarPedidos().subscribe(pedidos => {
-      console.log(pedidos)
+    this.camareroSvc.cargarPedidos().subscribe((pedidos) => {
       // añadir al array el nuevo array solo los cambios. Por ejem si se añade un pedido nuevo o si se elimina
       this.comprobarCambiosPedido(pedidos);
-      this.marcarMesaComoOcupadas()
-      
+      this.marcarMesaComoOcupadas();
+
       // lo mismo que en el dashboard de cocinero, el nombre del usuario y de las comidas tendremos que recuperarlas de rest
-      this.pedidos.forEach(pedido => {
-        const $user = this.rest.obtenerClienteId(pedido.usuarioId)
-        $user.subscribe((data: ICliente) => pedido.nombreUsuario = data.nombre)
+      this.pedidos.forEach((pedido) => {
+        const $user = this.rest.obtenerClienteId(pedido.usuarioId);
+        $user.subscribe(
+          (data: ICliente) => (pedido.nombreUsuario = data.nombre)
+        );
 
-        pedido.comidas.forEach(comida => {
-          const $comida = this.rest.obtenerComida(comida.comidaId)
-          $comida.subscribe((data) => comida.nombreComida = data.nombre)
-        })
-      })  
+        pedido.comidas.forEach((comida) => {
+          const $comida = this.rest.obtenerComida(comida.comidaId);
+          $comida.subscribe((data) => (comida.nombreComida = data.nombre));
+        });
+      });
 
-
-      
-    })
+    });
   }
+
+  
+
 
   /**
    * metodo para actualizar el pedido sin tenerlo que recargar todo de nuevo
-   * @param pedidos 
+   * @param pedidos
    */
-  comprobarCambiosPedido(pedidos:Pedido[]) {
-    const nuevosPedidos = pedidos.filter(pedido => !this.pedidos.includes(pedido));
+  comprobarCambiosPedido(pedidos: Pedido[]) {
+    const nuevosPedidos = pedidos.filter(
+      (pedido) => !this.pedidos.includes(pedido)
+    );
     this.pedidos.push(...nuevosPedidos);
 
     // 2º paso: comprobar si hay pedidos que ya no están
-    const pedidosEliminados = this.pedidos.filter(pedido => !pedidos.includes(pedido));
-    pedidosEliminados.forEach(pedido => {
+    const pedidosEliminados = this.pedidos.filter(
+      (pedido) => !pedidos.includes(pedido)
+    );
+    pedidosEliminados.forEach((pedido) => {
       const index = this.pedidos.indexOf(pedido);
       this.pedidos.splice(index, 1);
     });
@@ -75,36 +85,34 @@ export class CamareroDashboardComponent {
 
   marcarMesaComoOcupadas() {
     // cada pedido tiene marcadio una mesa, está marcarla
-    this.pedidos.forEach(pedido => {
-      this.mesasOcupadas.push(pedido.mesa)
-    })
+    this.pedidos.forEach((pedido) => {
+      this.mesasOcupadas.push(pedido.mesa);
+    });
   }
 
-  comprobarMesaOcupada(mesa:number):boolean {
-    return this.mesasOcupadas.includes(mesa)
+  comprobarMesaOcupada(mesa: number): boolean {
+    return this.mesasOcupadas.includes(mesa);
   }
 
-  getComidasByMesa(mesa:number): ComidaPedido[] {
-    const pedido = this.pedidos.find(pedido => pedido.mesa === mesa)
-    return pedido?.comidas || []
+  getComidasByMesa(mesa: number): ComidaPedido[] {
+    const pedido = this.pedidos.find((pedido) => pedido.mesa === mesa);
+    return pedido?.comidas || [];
   }
 
   /**
-   * metodo para marcar un pedido como completado 
+   * metodo para marcar un pedido como completado
    * @param mesa index de la mesa señalada
    */
-  marcarPedidoCompletado(mesa:number) {
-    const pedido = this.pedidos.find(pedido => pedido.mesa === mesa)
-    pedido!.estado = 'Pendiente Pago'
-    // enviar a rest para actualizar el estado 
-    this.camareroSvc.actualizarPedido(pedido!).subscribe(
-      (data) => {
-          this.toastr.success('Pedido completado')
-          // eliminar el pedido de la lista
-          // volvemos a setear el numero a la mesa en vez del nombre de la mesa
-          this.mesasOcupadas = this.mesasOcupadas.filter(m => m !== mesa)
-          this.getPedidos()
-      }
-    )
+  marcarPedidoCompletado(mesa: number) {
+    const pedido = this.pedidos.find((pedido) => pedido.mesa === mesa);
+    pedido!.estado = "Pendiente Pago";
+    // enviar a rest para actualizar el estado
+    this.camareroSvc.actualizarPedido(pedido!).subscribe((data) => {
+      this.toastr.success("Pedido completado");
+      // eliminar el pedido de la lista
+      // volvemos a setear el numero a la mesa en vez del nombre de la mesa
+      this.mesasOcupadas = this.mesasOcupadas.filter((m) => m !== mesa);
+      this.getPedidos();
+    });
   }
 }
